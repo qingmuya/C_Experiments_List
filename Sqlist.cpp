fb25e7aa22d68ce9e5b21daf92ab65879ea0327e
yuhaiyuan <lzqm04@gmail.com>
2023-09-22 09:02:16 +0800

    全部完成，重构部分代码

--- Sqlist.cpp
+++ Sqlist.cpp
@@ -10,38 +10,48 @@ void InitSqlist(Sqlist &L){
     L.length = 0;
 }
 
+//顺序表输入函数
+void Input(Sqlist &L){
+    int x;//用于确定输入数组的长度
+    printf("输入本数组的长度：\n");
+    scanf("%d",&x);
+    printf("输入本数组的%d个数值，以空格间隔：\n",x);
+    for(int i = 0;i < x;i++){
+        scanf("%d",&L.number[i]);
+    }
+    L.length += x;
+}
+
 //顺序表合并
-void Sqlist_merge(){
-    Sqlist A;
-    InitSqlist(A);
-    Sqlist B;
-    InitSqlist(B);
+void Sqlist_merge(Sqlist &A,Sqlist &B){
+    //C用于转存A和B合并的结果
     Sqlist C;
     InitSqlist(C);
-
-    for(int i = 0;i < 4;i++){
-        int a;
-        printf("输入顺序表A的第%d个元素（共四个元素）:\n",i+1);
-        scanf("%d",&a);
-        A.number[i] = a;
-        A.length++;
-    }
-
-    for(int i = 0;i < 3;i++){
-        int b;
-        printf("输入顺序表B的第%d个元素（共三个元素）:\n",i+1);
-        scanf("%d",&b);
-        B.number[i] = b;
-        B.length++;
-    }
-
-    for(int i = 0;i < B.length;i++){
-        A.number[i + 4] = B.number[i];
-        A.length++;
-    }
-
+    //A的值转存进C
     for(int i = 0;i < A.length;i++){
-        printf("%d ",A.number[i]);
+        C.number[i] = A.number[i];
+        C.length++;
+    }
+    //将B的值转存进C
+    for(int i = 0;i < B.length;i++){
+        //用于判断是否重复
+        int choose = 0;
+        //遍历C，判断是否有重复
+        for(int j = 0;j < C.length;j++){
+            if(B.number[i] == C.number[j]){
+                choose++;
+                break;
+            }
+        }
+        //根据choose判断是否插入
+        if(choose == 0){
+            C.number[C.length] = B.number[i];
+            C.length++;
+        }
+    }
+    //打印输出结果
+    for(int i = 0;i < C.length;i++){
+        printf("%d ",C.number[i]);
     }
 }
 
@@ -54,71 +64,72 @@ void InitSqlistdata(data L[],int x){
 
 //向多项式数组添加值
 int Adddata(data L[]){
-    int i = 1,js = 1,last;
-    while(i){
-        printf("输入第%d项的值：\n",js);
-        float c = 0;   //系数
-        int e = 0;   //指数
-        printf("输入该项的指数：\n");
-        scanf("%d",&e);
-        last = e;
-        printf("输入该项的系数：\n");
-        scanf("%f",&c);
+    //x限制接受多项式项数，e、c存储输入的系数和指数
+    int x,e,Max = 0;
+    float c;
+    printf("请输入该多项式的项数：\n");
+    scanf("%d",&x);
+    printf("请输入多项式每项的系数及指数，以空格间隔：\n");
+    for(int i = 0;i < x;i++){
+        scanf("%f %d",&c,&e);
         L[e].coef = c;
-        js++;
-        printf("结束输入请输入 0 ，继续输入请输入 1 ：\n");
-        scanf("%d",&i);
+        //保存最大指数
+        if(e > Max){
+            Max = e + 1;
+        }
     }
-    //printf("coef = %f  expn = %d\n",L[last].coef,last);
-    return last;
+    return Max;
 }
 
-//相加，存入C，打印
+//顺序表多项式相加，存入C，打印
 void SqlistdataSum(data A[],data B[],int A_max,int B_max){
     int C_max;
-    data C[20];
-    InitSqlistdata(C,20);
+    //判断最大指数
     if(A_max >= B_max){
         C_max = A_max;
     }
     else{
         C_max = B_max;
     }
-    for(int i = 0;i <= A_max;i++){
-        C[i].coef = C[i].coef + A[i].coef;
+    //定义多项式数组C
+    data C[C_max];
+    InitSqlistdata(C,C_max);
+    //合并同类项
+    for(int i = 0;i < A_max;i++){
+        C[i].coef += A[i].coef;
     }
     for(int i = 0;i <= B_max;i++){
-        C[i].coef = C[i].coef + B[i].coef;
+        C[i].coef += B[i].coef;
     }
+    //打印输出
     dataPrint(C,C_max);
 }
 
 void dataPrint(data L[],int Max){
-    int sf = 0;
-    //判断指数为0、1时，打印特殊值
+    //计数器，用于判断首个项
+    int jsq = 0;
+    //首项为常数项
     if(L[0].coef != 0){
-        printf("%f",L[0].coef);
-        sf++;
+        printf("%f ",L[0].coef);
+        jsq++;
     }
-    if(L[1].coef > 0){
-        printf("+%fx",L[0].coef);
-        sf++;
+    //首项的其他情况
+    if(L[1].coef > 0 && jsq != 0){
+        printf("+ %fx ",L[1].coef);
     }
-    else if(L[1].coef < 0){
-        printf("%fx",L[0].coef);
-        sf++;
+    else if(L[1].coef < 0 && jsq != 0){
+        printf("- %fx ",-L[1].coef);
     }
-    //循环判断即可
-    for(int i = 2;i <= Max;i++){
-        if(sf == 0 && L[i].coef != 0){
-            printf("%fx^%d",L[i].coef,i);
-            sf++;
+    else if(L[1].coef != 0 && jsq == 0){
+        printf("%fx ",L[1].coef);
+    }
+    //普遍情况
+    for(int i = 2;i < Max;i++){
+        if(L[i].coef > 0){
+            printf("+ %fx^%d ",L[i].coef,i);
         }
-        else if(L[i].coef > 0 && sf == 1){
-            printf("+%fx^%d",L[i].coef,i);
-        }
-        else if(L[i].coef < 0 && sf == 1){
-            printf("%fx^%d",L[i].coef,i);
+        else if(L[i].coef < 0){
+            printf("- %fx^%d ",-L[i].coef,i);
         }
     }
 }
--- Sqlist.h
+++ Sqlist.h
@@ -19,15 +19,18 @@ typedef struct{
     int expn;   //指数
 }data;
 
+//顺序表初始化
 void InitSqlist(Sqlist &L);
-
-void Sqlist_merge();
-
+//顺序表输入
+void Input(Sqlist &L);
+//顺序表合并
+void Sqlist_merge(Sqlist &A,Sqlist &B);
+//多项式顺序表初始化
 void InitSqlistdata(data L[],int x);
-
+//多项式顺序表添加数值
 int Adddata(data L[]);
-
+//多项式顺序表求和
 void SqlistdataSum(data A[],data B[],int A_max,int B_max);
-
+//多项式顺序表打印
 void dataPrint(data L[],int C_max);
 #endif // LINK_LIST_H_INCLUDED
